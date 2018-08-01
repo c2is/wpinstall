09683f13f2f8fd31ee6154dedf67550d21004a86
André Cianfarani <acianfa@gmail.com>
2018-08-01 16:48:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Wordpress installer
+
+### Installation
+
+#### Linux/bsd
+```sh
+curl -sL http://gitlab.c2is.fr/wpinstall/raw/master/wpinstall.sh --output /usr/local/bin/wpinstall; chmod +x /usr/local/bin/wpinstall;
+
+```
+
+#### Windows mingw
+```sh
+mkdir ~/bin/; curl -sL http://gitlab.c2is.fr/wpinstall/raw/master/wpinstall.sh --output ~/bin/wpinstall; chmod +x ~/bin/wpinstall
+```
---
+++ wpinstall.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#####
+dir=$1
+install_path=$(pwd)/$dir"/web"
+
+function gitignore () {
+	echolor y "Mise en place du gitignore dans "$install_path
+	cat << EOF > $install_path"/.gitignore"
+*.log
+wp-config.php
+wp-content/advanced-cache.php
+wp-content/backup-db/
+wp-content/backups/
+wp-content/blogs.dir/
+wp-content/cache/
+wp-content/upgrade/
+wp-content/uploads/
+EOF
+}
+
+function conffiles () {
+	echolor y "Mise en place des fichiers de configutations \"-at-preprod\" et \"-at-prod\" dans "$install_path
+	cp $install_path"/wp-config-sample.php" $install_path"/wp-config.php-at-preprod"
+	cp $install_path"/wp-config-sample.php" $install_path"/wp-config.php-at-prod"
+}
+
+function echolor () {
+	red="\033[31m"
+	green='\033[32m'
+	yellow='\033[33m'
+	std="\033[0m"
+	case $1 in
+		r)
+		  color=$red
+		  ;;
+		g)
+		 color=$green$
+		  ;;
+		y)
+		  color=$yellow
+		  ;;
+		s)
+		  color=$std
+		  ;;
+
+	esac
+
+	echo -e $color$2$std
+}
+
+if [ "$dir" == "" ]; then
+	read -p "Vous n'avez pas indiqué de répertoire d'installation, Wordpress sera installé ici : "$install_path", ok [Y,n] : " yn
+	if [[ ! $yn =~ ^[Yy]$ ]]
+	then
+	    echo "Ok, ok... on arrête tout."
+	    exit 1
+	fi
+fi
+if [ -d $dir ] || [ -f $dir ]; then echo "Ce répertoire existe déjà, on arrête tout."; exit 1; fi
+
+mkdir -p $dir"/web"
+cd $install_path;
+
+echolor y "Récupération de Wordpress dans "$install_path
+curl -L http://wordpress.org/latest.tar.gz --output latest.tar.gz
+
+echolor y "Décompression de Wordpress dans "$install_path
+tar -zxf latest.tar.gz --strip 1
+
+
+gitignore
+
+conffiles
